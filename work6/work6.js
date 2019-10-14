9679bac703327b30c2ba5c00cef899ce1d1b4ae4
panyyjk <1436208314@qq.com>
2019-10-14 22:22:51 +0800

    555

--- work6/work6.js
+++ work6/work6.js
@@ -1,40 +1,32 @@
-document.write("<p>九*九空表</p>")
-let level = prompt('九*九空表层数')
-level = parseFloat(level) && Number(level)
-if (isNaN(level)) {
-    alert('九*九空表层数是数字')
+str = '<table border="1">'
+for (let i = 1; i < 10; ++i) {
+    str += '<tr>'
+    for (var j = 1; j < 10; ++j) {
+        str += '<td>&nbsp;</td>'
+    }
+    str += '</ty>'
 }
-for (let i = 1; i <10; ++i) {
-    let blank = level - i;
-    for (let k = 0; k < blank; k) {
-        document.write('&nbsp');
-    }
-    let star = i * 2 - 1
-    for (let j = 0; j < star; ++j) {
-        document.write("<br>")
-    }
-    let str = '<table boder="1">'
-    for (let i = 1; i < 10; ++i) {
-        str += '<tr>'
-        for (var j = 1; j <= i; ++j) {
-            str += '<td>' + j + '*' + i + '=' + (j * i) + '</td>'
-        }
-        str += '</tr>'
-    }
-    str += '</table>'
-    document.getElementById('table').innerHTML = str
+str += '</table>'
+document.getElementById('table1').innerHTML = str
 
-
+str = '<table border="1">'
+for (let i = 9; i > 0; --i) {
+    str += '<tr>'
+    for (var j = i; j > 0; --j) {
+        str += '<td>&nbsp;</td>'
+    }
+    str += '</ty>'
 }
+str += '</table>'
+document.getElementById('table2').innerHTML = str
 
-
-
-
-
-
-
-
-
-
-
-
+str = '<table border="1">'
+for (let i = 9; i > 0; --i) {
+    str += '<tr>'
+    for (var j = i; j > 0; --j) {
+        str += '<td>' + j + '*' + i + '=' + (i * j) + '</td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
