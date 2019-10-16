580a1780e3c6976a89c5d0270c0136522034e939
panyyjk <143620814@qq.com>
2019-10-16 15:43:16 +0800

    47

---
+++ work7/pai.js
@@ -0,0 +1,34 @@
+function calculate(){
+let num = document.getElementById('num').value
+num = parseInt(num)&&Number(num)
+if(!isNaN(num)){
+    document.getElementById('num').value = pi(num)
+ }
+}
+function factorial(n){
+    let num = 1 
+    for(let i=1;i<=n;++i){
+        screen
+    }
+    return sum
+}
+
+
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1;i<=n;++i){
+        screen
+    }
+    return sum
+}
+function pi(num) {
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial( )
+        let divisor = oddFactorial(n)
+     SVGFEFuncGElement   
+    }
+    return screen
+}
+
+calculate()
