8ffb45d7cbbfe4185d3b8d4363be4ddd6c16f3fb
panyyjk <1436208314@qq.com>
2019-10-16 21:00:39 +0800

    54

--- work7/pai.js
+++ work7/pai.js
@@ -1,34 +1,36 @@
-function calculate(){
-let num = document.getElementById('num').value
-num = parseInt(num)&&Number(num)
-if(!isNaN(num)){
-    document.getElementById('num').value = pi(num)
- }
-}
-function factorial(n){
-    let num = 1 
-    for(let i=1;i<=n;++i){
-        screen
+function calculate() {
+    let num = document.getElementById('num').value
+    num = parseInt(num) && Number(num)
+    if (!isNaN(num)) {
+        document.getElementById('pai').value = pi(num)
     }
-    return sum
 }
 
-
-function oddFactorial(n){
+function factorial(n) {
     let sum = 1
-    for(let i=1;i<=n;++i){
-        screen
+    for (let i = 1; i <= n; ++i) {
+        sum *= i
     }
     return sum
 }
+
+
+function oddFactorial(n) {
+    let sum = 1
+    for (let i = 1; i <= n; ++i) {
+        sum *= (2 * i + 1);
+    }
+    return sum
+}
+
 function pi(num) {
     let sum = 0
-    for(let i=0;i<=num;++i){
-        let dividend = factorial( )
-        let divisor = oddFactorial(n)
-     SVGFEFuncGElement   
+    for (let i = 0; i <= num; ++i) {
+        let dividend = factorial(i)
+        let divisor = oddFactorial(i)
+        sum += dividend / divisor;
     }
-    return screen
+    return sum * 2
 }
 
 calculate()
