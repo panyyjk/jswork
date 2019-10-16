a5ffad6a594eeb91890e65e47bf7148e0a795c8b
panyyjk <1436208314@qq.com>
2019-10-16 21:08:03 +0800

    12

--- work7/tarray.js
+++ work7/tarray.js
@@ -1,17 +1,11 @@
-var arr = [1,2,3,4,5,6,7,8,9,2,2,4,3,2];
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 2, 4, 3, 2];
 document.getElementById('array').innerHTML = arr.toString()
-var index = 0,newArr = [];
-for(var i in arr){
-    if(arr[i] !=2){
+var index = 0,
+    newArr = [];
+for (var i in arr) {
+    if (arr[i] != 2) {
         newArr[index] = arr[i];
-    ++index;
+        ++index;
     }
 }
 document.getElementById('transArray').innerHTML = newArr.toString()
-
-
-
-
-
-
-
