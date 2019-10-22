c4c62347943444429888871d0ba60f73fe7c8f58
panyyjk <1436208314@qq.com>
2019-10-22 23:21:38 +0800

    899

--- work8/index.js
+++ work8/index.js
@@ -1,11 +1,11 @@
-function sort(method) {
+function sort(method){
     let origin = document.getElementById('origin').value
     let logs
     console.log(method)
-    if (method == 'bubble') {
+    if(method=='bubble'){
         logs = bubbleSort(origin)
-    } else if (method == 'insert') {
-        logs = insertSoet(origin)
+    }else if(method=='insert'){
+        logs = insertSort(origin)
     }
     showDiv = document.getElementById('Anim')
     showDiv.innerHTML = logs
--- work8/utils.js
+++ work8/utils.js
@@ -14,7 +14,7 @@ function insertSort(str) {
     for (let i = 1; i < arr.length; i++) {
         for (let j = i; j > 0; j--) {
             if (arr[j - 1] > arr[j]) {
-                [arr[j - 1], arr[j] = [arr[j], arr[j - 1]]]
+                [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
             }
         }
     }
