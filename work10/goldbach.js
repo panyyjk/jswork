c75e9ac0238111f8b2c4303fb7f47f1fa6a1c8be
panyyjk <1436208314@qq.com>
2019-10-24 10:10:03 +0800

    456

--- work10/goldbach.js
+++ work10/goldbach.js
@@ -1,12 +1,30 @@
-function calc(func){
-    let result = document.getElementById('result')
-    let num = parseInt(document.getElementById('num').value)
-    if(num % 2){
-        alert('请输入偶数')
-        return false
+    let odd = document.getElementById('odd').value
+    odd = parseInt(odd);
+    let str = '';
+
+    function pan(num) {
+        var sum = 0;
+        for (var d = 1; d <= num; d++) {
+            if (num % d == 0) {
+                sum++;
+            }
+        }
+        if (sum == 2) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    result.value = func(num)
-}
-function odd(num){
-    
-}
+
+    function goldbach(odd) {
+        odd = document.getElementById('odd').value
+        odd = parseInt(odd);
+
+        for (var b = 2; b < odd; b++) {
+            var c = odd - b;
+            if (pan(b) && pan(c) && b <= c) {
+                str += ("偶数" + odd + "可以拆分成质数" + b + "和" + c + '<br>');
+            }
+        }
+        document.getElementById('goldbach').innerHTML = str;
+    }
