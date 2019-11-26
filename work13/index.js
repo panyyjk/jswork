0220779aea2ffa3512d43808732ae35b6d74c296
panyyjk <1436208314@qq.com>
2019-11-26 23:26:03 +0800

    work12

--- work13/index.js
+++ work13/index.js
@@ -1,5 +1,6 @@
-let year = new Date().getFullYear()
-document.getElementById("cal").innerHTML = calendar(year)
-function setCalendar(year) {
-
+var year = new Date().getFullYear()
+    document.getElementById("cal").innerHTML = calendar(year)
+    
+function setCalendar(year){
+    
 }
