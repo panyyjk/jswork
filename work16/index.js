746c9032a01038ed1f2dd9eb99d5f38542f7f0dc
panyyjk <1436208314@qq.com>
2019-12-10 22:31:41 +0800

    work15

---
+++ work16/index.js
@@ -0,0 +1,21 @@
+ShopCart('cart', [{
+    name: 'web前端开发中级 (上册)',
+    price: 99.0,
+    num: 2
+},
+{
+    name: 'web前端开发中级 (下册)',
+    price:88.0,
+    num:2
+},
+{
+    name: 'web前端开发高级 (上册)',
+    price: 77.0,
+    num: 3
+},
+{
+    name: 'web前端开发高级 (下册)',
+    price: 66.0,
+    num: 3
+}
+]);
